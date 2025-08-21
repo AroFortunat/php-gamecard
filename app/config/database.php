2a5239ddacee70b795b279bc1596e297ac859b6e
AroFortunat <aro.fortunat.ralaiseheno@esti.mg>
2025-08-21 22:34:20 +0300

    refactor: update database connection handling and remove unused index.php

--- app/config/database.php
+++ app/config/database.php
@@ -1,8 +1,21 @@
 <?php 
-$servername =  getenv('HOST_MACHINE_SERVER_NAME');
-$dbUsername = getenv('MYSQL_QUIZDB_USER');
-$dbPassword = getenv('MYSQL_QUIZDB_PASSWORD');
-$dbName = getenv('MYSQL_QUIZDB');
 
-echo $servername;
+require('../../vendor/autoload.php');
+
+$rootPath = dirname(__DIR__, 2); // Remonte de 2 niveaux
+
+// Charge le fichier .env à la racine du projet
+$dotenv = Dotenv\Dotenv::createImmutable($rootPath);
+$dotenv->load();
+
+$servername =  $_ENV['HOST_MACHINE_SERVER_NAME'];
+$dbUsername = $_ENV['MYSQL_QUIZDB_USER'];
+$dbPassword = $_ENV['MYSQL_QUIZDB_PASSWORD'];
+$dbName = $_ENV['MYSQL_QUIZDB'];
+
+$connex = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
+if (!$connex) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
 ?>
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$servername =  getenv('HOST_MACHINE_SERVER_NAME');
-$dbUsername = getenv('MYSQL_QUIZDB_USER');
-$dbPassword = getenv('MYSQL_QUIZDB_PASSWORD');
-$dbName = getenv('MYSQL_QUIZDB');
-
-echo $servername;
-?>
