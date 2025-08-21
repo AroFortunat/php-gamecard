46b0db6e23bd7162c0afdbcce2105f52a24c58c5
AroFortunat <aro.fortunat.ralaiseheno@esti.mg>
2025-08-21 20:49:35 +0300

    add:composer

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+
+$servername =  getenv('HOST_MACHINE_SERVER_NAME');
+$dbUsername = getenv('MYSQL_QUIZDB_USER');
+$dbPassword = getenv('MYSQL_QUIZDB_PASSWORD');
+$dbName = getenv('MYSQL_QUIZDB');
+
+echo $servername;
+?>
